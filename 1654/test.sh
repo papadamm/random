33d0ac8a0ba9e3576969412ae85acf1aa6d68bad
papadamm <magnus.damm@gmail.com>
2026-09-15 18:55:54 +0200

    use binary data for testing, test hex decoder+encoder first

--- 1654/test.sh
+++ 1654/test.sh
@@ -12,24 +12,55 @@ hex_tests="encode-x16:decode-xX16 encode-X16:decode-xX16"
 fivefour_single_tests="encode-54s:decode-54s encode-54s:decode-54ds"
 fivefour_dual_tests="encode-54d:decode-54d encode-54d:decode-54ds"
 
-output_256 ()
+output_256_ascii ()
 {
-  for e in 0 `seq 255`
-  do
-    echo -ne `echo -ne 0; bc -l -e "hex($e)"` | tail -c 2
-  done
+    for e in 0 `seq 255`
+    do
+        echo 0 `bc -l -e "hex($e)"` | tr -d "[:space:]" | tail -c 2
+    done
 }
 
-reference_output=`output_256`
+echo_pass_fail_exit ()
+{
+    if [ $1 -eq 0 ]; then
+        echo PASS
+    else
+        echo FAIL
+        exit 1
+    fi
+}
+
+# test decoding the ASCII stream first, execution only test
+/bin/echo -n "decode-xX16 "
+output_256_ascii | ./1654 decode-xX16 > /dev/null
+echo_pass_fail_exit $?
+
+# test encoding the binary data back to ASCII, execution only test
+/bin/echo -n "encode-X16 "
+output_256_ascii | ./1654 decode-xX16 | ./1654 encode-X16 > /dev/null
+echo_pass_fail_exit $?
+
+reference_output=`output_256_ascii`
+
+# test both hex decoder and encoder together, compare results
+/bin/echo -n "decode-xX16:encode-X16 "
+test_output=`output_256_ascii | ./1654 decode-xX16 | ./1654 encode-X16`
+test "$test_output" == "$reference_output"
+echo_pass_fail_exit $?
+
+output_256_bin ()
+{
+    /bin/echo -n "$reference_output" | ./1654 decode-xX16
+}
 
 for m in $hex_tests $fivefour_single_tests $fivefour_dual_tests
 do
+    /bin/echo -n "$m "
+    
     e=`echo $m | cut -d ":" -f 1`
     d=`echo $m | cut -d ":" -f 2`
-    test_output=`output_256 | ./1654 $e | ./1654 $d`
-    if [ "$test_output" == "$reference_output" ]; then
-	echo $m PASS
-    else
-	echo $m FAIL
-    fi
+    test_output=`output_256_bin | ./1654 $e | ./1654 $d | ./1654 encode-X16`
+
+    test "$test_output" == "$reference_output"
+    echo_pass_fail_exit $?
 done
